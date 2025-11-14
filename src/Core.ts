04541368f919e2878bab827791d1757c55610461
Joaquín Cuitiño <3134745+Vextil@users.noreply.github.com>
2025-11-13 23:51:33 -0300

    I'm thinking I'm back

--- src/Core.ts
+++ src/Core.ts
@@ -21,20 +21,11 @@ export default class Core {
   public static scene = new THREE.Scene();
 
   private static stats = new Stats();
-  private static statsRender = new Stats.Panel("FPS", "#ff8", "#221");
-  private static statsRenderTime = new Stats.Panel("ms", "#ff8", "#221");
-
-  private static maxRender = 0;
-  private static maxRenderTime = 0;
 
   private static renderClock = new THREE.Clock(true);
-  private static renderTimeClock = new THREE.Clock();
 
   static init() {
     document.body.appendChild(this.stats.dom);
-    this.stats.showPanel(3);
-    this.stats.addPanel(this.statsRender);
-    this.stats.addPanel(this.statsRenderTime);
 
     this.camera = new Camera();
     this.renderer = new Renderer();
@@ -52,25 +43,13 @@ export default class Core {
     requestAnimationFrame(() => this.renderUpdate());
 
     let deltaTime = this.renderClock.getDelta();
-    if (deltaTime > this.maxRender) {
-      this.maxRender = deltaTime;
-    }
-    if (deltaTime > 0) {
-      this.statsRender.update(1 / deltaTime, 150);
-    }
-    this.renderTimeClock.start();
 
     this.player.update(deltaTime);
     this.physics.step(deltaTime);
     this.camera.update(deltaTime);
     await this.renderer.update();
 
-    this.renderTimeClock.stop();
-    const time = this.renderTimeClock.getElapsedTime() * 1000;
-    if (time > this.maxRenderTime) {
-      this.maxRenderTime = time;
-    }
-    this.statsRenderTime.update(time, this.maxRenderTime);
+    this.stats.update();
   }
 }
 
--- src/Multiplayer.ts
+++ src/Multiplayer.ts
@@ -1,27 +1,27 @@
-import * as Colyseus from "colyseus.js";
-import type { RoomState } from "../server/src/rooms/schema/RoomState";
+// import * as Colyseus from "colyseus.js";
+// import type { RoomState } from "../server/src/rooms/schema/RoomState";
 
 export default class Multiplayer {
-  public client: Colyseus.Client;
+  // public client: Colyseus.Client;
 
   constructor() {
-    this.client = new Colyseus.Client("ws://localhost:2567");
-    this.client
-      .joinOrCreate<RoomState>("game_room")
-      .then((room) => {
-        room.onStateChange((state) => {
-          console.log(room.name, "has new state:", state);
-        });
-        room.onMessage("*", (message) => {
-          console.log(room.sessionId, "received on", room.name, message);
-        });
-        room.onError((code, message) => {
-          console.log(room.sessionId, "couldn't join", room.name, code, message);
-        });
-        console.log(room.sessionId, "joined", room.name, room.id);
-      })
-      .catch((e) => {
-        console.log("JOIN ERROR", e);
-      });
+    // this.client = new Colyseus.Client("ws://localhost:2567");
+    // this.client
+    //   .joinOrCreate<RoomState>("game_room")
+    //   .then((room) => {
+    //     room.onStateChange((state) => {
+    //       console.log(room.name, "has new state:", state);
+    //     });
+    //     room.onMessage("*", (message) => {
+    //       console.log(room.sessionId, "received on", room.name, message);
+    //     });
+    //     room.onError((code, message) => {
+    //       console.log(room.sessionId, "couldn't join", room.name, code, message);
+    //     });
+    //     console.log(room.sessionId, "joined", room.name, room.id);
+    //   })
+    //   .catch((e) => {
+    //     console.log("JOIN ERROR", e);
+    //   });
   }
 }
